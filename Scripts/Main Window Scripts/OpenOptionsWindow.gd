14c08d32c0c45a4baff44dfe7c4bc56f83971f7e
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-03-19 13:35:51 +0100

    window now called using "popup()" again

--- Scripts/Main Window Scripts/OpenOptionsWindow.gd
+++ Scripts/Main Window Scripts/OpenOptionsWindow.gd
@@ -6,7 +6,11 @@ func _pressed():
 	var window = windowScene.instantiate()
 	var sceneRoot = self.owner
 	sceneRoot.add_child(window)
-	window.show()
+
+	# the proper method to open it would be window.show(). 
+	# But if i do that, the window wont be exclusive anymore.
+	# So i guess we'll just take this one even if it throws an error
+	window.popup()
 
 
 
