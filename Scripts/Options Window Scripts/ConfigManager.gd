3ce20f55689e52680148e427509e238a0f9c707f
Thane5 <13683581+Thane5@users.noreply.github.com>
2023-03-21 20:15:47 +0100

    check if config is null before doing anything stupid

--- Scripts/Options Window Scripts/ConfigManager.gd
+++ Scripts/Options Window Scripts/ConfigManager.gd
@@ -36,54 +36,56 @@ func load_config_and_set_values():
 	
 	%ConfigLoader.load_config()
 	
-	# DISPLAY CATEGORY GETTERS
-	windowModeDropdown.selected = config.windowMode
-	vsyncCheckbox.button_pressed = config.vsync
-	limitFPSCheckbox.button_pressed = config.limitFPS
-	maxFPSSpinbox.value = config.maxFPS
-	
-	# GRAPHICS CATEGORY GETTERS
-	antiAliasingCheckbox.button_pressed = config.antiAliasing
-	shadowsCheckbox.button_pressed = config.shadows
-	
-	# AUDIO CATEGORY GETTERS
-	musicSlider.value = config.music
-	effectsSlider.value = config.effects
-	voicesSlider.value = config.voices
-	
-	# GAMEPLAY CATEGORY GETTERS
-	subtitlesCheckbox.button_pressed = config.subtitles
-	autodefendCheckbox.button_pressed = config.autoDefend
-	
-	# FILEPATHS CATEGORY GETTERS
-	redguardPathLineEdit.text = config.redguardPath
-	
-	%DependencyManager.update_vsync_dependencies()
-	%DependencyManager.update_fpslimit_dependencies()
+	if config != null:
+		# DISPLAY CATEGORY GETTERS
+		windowModeDropdown.selected = config.windowMode
+		vsyncCheckbox.button_pressed = config.vsync
+		limitFPSCheckbox.button_pressed = config.limitFPS
+		maxFPSSpinbox.value = config.maxFPS
+		
+		# GRAPHICS CATEGORY GETTERS
+		antiAliasingCheckbox.button_pressed = config.antiAliasing
+		shadowsCheckbox.button_pressed = config.shadows
+		
+		# AUDIO CATEGORY GETTERS
+		musicSlider.value = config.music
+		effectsSlider.value = config.effects
+		voicesSlider.value = config.voices
+		
+		# GAMEPLAY CATEGORY GETTERS
+		subtitlesCheckbox.button_pressed = config.subtitles
+		autodefendCheckbox.button_pressed = config.autoDefend
+		
+		# FILEPATHS CATEGORY GETTERS
+		redguardPathLineEdit.text = config.redguardPath
+		
+		%DependencyManager.update_vsync_dependencies()
+		%DependencyManager.update_fpslimit_dependencies()
 
 
 func set_values_and_save_config():
 	
-	# DISPLAY CATEGORY SETTERS
-	config.windowMode = windowModeDropdown.selected
-	config.vsync = vsyncCheckbox.button_pressed
-	config.limitFPS = limitFPSCheckbox.button_pressed
-	config.maxFPS = int(maxFPSSpinbox.value)
+	if config != null:
+		# DISPLAY CATEGORY SETTERS
+		config.windowMode = windowModeDropdown.selected
+		config.vsync = vsyncCheckbox.button_pressed
+		config.limitFPS = limitFPSCheckbox.button_pressed
+		config.maxFPS = int(maxFPSSpinbox.value)
 
-	# GRAPHICS CATEGORY SETTERS
-	config.antiAliasing = antiAliasingCheckbox.button_pressed
-	config.shadows = shadowsCheckbox.button_pressed
-	
-	# AUDIO CATEGORY SETTERS
-	config.music = int(musicSlider.value)
-	config.effects = int(effectsSlider.value)
-	config.voices = int(voicesSlider.value)
-	
-	# GAMEPLAY CATEGORY SETTERS
-	config.subtitles = subtitlesCheckbox.button_pressed
-	config.autoDefend = autodefendCheckbox.button_pressed
-	
-	# FILEPATHS CATEGORY SETTERS
-	config.redguardPath = redguardPathLineEdit.text
-	
-	%ConfigSaver.save_config()
+		# GRAPHICS CATEGORY SETTERS
+		config.antiAliasing = antiAliasingCheckbox.button_pressed
+		config.shadows = shadowsCheckbox.button_pressed
+		
+		# AUDIO CATEGORY SETTERS
+		config.music = int(musicSlider.value)
+		config.effects = int(effectsSlider.value)
+		config.voices = int(voicesSlider.value)
+		
+		# GAMEPLAY CATEGORY SETTERS
+		config.subtitles = subtitlesCheckbox.button_pressed
+		config.autoDefend = autodefendCheckbox.button_pressed
+		
+		# FILEPATHS CATEGORY SETTERS
+		config.redguardPath = redguardPathLineEdit.text
+		
+		%ConfigSaver.save_config()
